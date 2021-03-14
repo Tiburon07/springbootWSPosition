2caf4cba2f9a02a9026c9b01214c542581995399
t.iordache <t.iordache@geosystems.it>
2021-03-14 12:46:58 +0100

    modifica posizione

--- src/main/java/net/javaguides/springboot/websocket/controller/GreetingController.java
+++ src/main/java/net/javaguides/springboot/websocket/controller/GreetingController.java
@@ -3,7 +3,7 @@ package net.javaguides.springboot.websocket.controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
+//import org.springframework.web.util.HtmlUtils;
 
 import net.javaguides.springboot.websocket.model.Greeting;
 import net.javaguides.springboot.websocket.model.HelloMessage;
@@ -12,8 +12,8 @@ import net.javaguides.springboot.websocket.model.HelloMessage;
 public class GreetingController {
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(1000); // simulated delay
-        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    public String greeting(String message) throws Exception {
+        //Thread.sleep(1000); // simulated delay
+        return message;
     }
 }
